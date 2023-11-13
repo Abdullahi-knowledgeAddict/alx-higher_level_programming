ffa0ac00874797f2c30a3beacf063b6e8fcafab8
MUROBII <usmanmurabi@gmail.com>
2023-11-13 16:46:59 +0100

    Generate an algorithm that adds two numbers

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a: int, b: int):
+    """Addition function
+
+    Args:
+        a: integer first
+        b: integer second
+
+    Returns:
+        The addition of a and b
+    """
+    return (a + b)
