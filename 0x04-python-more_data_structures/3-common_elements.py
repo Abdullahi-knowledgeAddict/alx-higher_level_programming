16d86c4303b2af985ad88e4dd05c8b2f1f0f5de8
Abdullahi-knowledgeAddict <ademolaadebola161@gmail.com>
2023-11-21 09:33:33 +0100

    Generate a new set with intersection of two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+        Gets a set of common element in two sets
+
+    Args:
+        set_1: As the name implies
+        set_2: Also as the name implies
+
+    Return:
+            The intersection of the set
+    """
+    # Note this can also be done with (set_1 &  set_2)
+    return {id_1 for id_1 in set_1 for id_2 in set_2 if id_1 == id_2}
