f8d11a33a3504329dbd32f874882a7c8daeaa415
Abdullahi-knowledgeAddict <ademolaadebola161@gmail.com>
2023-11-21 11:18:06 +0100

    Develop a function to determine the number of keys of dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+        Evaluate the number of keys in a dictionary
+
+    Args:
+        a_dictionary: The dictionary to be evaluated
+
+    Returns:
+            The number of key/The dictionary length
+    """
+    return len(a_dictionary)
