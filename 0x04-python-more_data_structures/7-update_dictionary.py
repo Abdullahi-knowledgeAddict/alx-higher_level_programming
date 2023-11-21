827579d746374c58a2d6e8f2961d1944a2a55744
Abdullahi-knowledgeAddict <ademolaadebola161@gmail.com>
2023-11-21 11:53:49 +0100

    Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """
+        Updates a Dictionary
+
+        Args:
+            a_dictionary: The dictionary
+                   value: as name implies
+                     key: as name implies
+
+        Return:
+                Modified Dictionary
+    """
+    a_dictionary[key] = value
+    return a_dictionary
