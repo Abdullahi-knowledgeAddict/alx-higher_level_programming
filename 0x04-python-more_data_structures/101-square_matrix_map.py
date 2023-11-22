9dfc70f3bf8581296b2ad9b09f7a73357ade4cb7
Abdullahi-knowledgeAddict <ademolaadebola161@gmail.com>
2023-11-22 09:55:46 +0100

    Write a func that computes the square of a matrix
    -> Prototype: def square_matrix_map(matrix=[]):
    -> matrix is a 2 dimensional array
    -> Returns a new matrix
    	-> Same size as matrix
    	-> Each value should be the square of the value of the input
    -> Initial matrix should not be modified
    -> You are not allowed to import any module
    -> You have to use map #python built-in function
    -> You are not allowd to use for or while loop
    -> Your file should be max 3 lines

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x*x, row)) for row in matrix]#map
+    return list(map(lambda row: list(map(lambda col: col**2, row)), matrix))
