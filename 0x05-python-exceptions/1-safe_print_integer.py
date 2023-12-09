763a80cc9672aa4b3c6919c502f90ac13f1f285f
knowledgeAddict <ademolaadebola161@gmail.com>
2023-12-09 14:50:22 +0100

    Make the previous functional more safer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -14,5 +14,5 @@ def safe_print_integer(value):
         x = int(value)
         print("{:d}".format(x))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
