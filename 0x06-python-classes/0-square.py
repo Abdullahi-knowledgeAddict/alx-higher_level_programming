b333846716dc6a2c8656db3ae7e2a8365fa9429c
knowledgeAddict <ademolaadebola161@gmail.com>
2023-12-20 15:40:12 +0100

    Scrutinize, to make module obey Google Style python Docstrings standards

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+# -*- coding: utf-8 -*-
+"""Documenting a module that contains an empty class
+   Called Square that does absolutely Nothing!
+    """
+
+
 class Square:
     """An Empty class that does nothing"""
     pass
