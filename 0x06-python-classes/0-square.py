3f5488ccedc99ee1417e57a6cbf9c559608dee38
knowledgeAddict <ademolaadebola161@gmail.com>
2024-01-11 12:43:44 +0100

    Write an empty class 'Square' that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 # -*- coding: utf-8 -*-
-"""Documenting a module that contains an empty class
-   Called Square that does absolutely Nothing!
-    """
+""" Modules  Documentation
+
+    This module is a simple and empty module
+"""
 
 
 class Square:
-    """An Empty class that does nothing"""
+    """Definition of an empty class"""
     pass
