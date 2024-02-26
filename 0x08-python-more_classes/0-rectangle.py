47c50b9e81b66220ffd97f969c3a5a957b77a0b6
knowledgeAddict <ademolaadebola161@gmail.com>
2024-02-26 09:24:53 +0100

    Implement an Empty Rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# -*- coding: utf-8 -*-
+"""A simple  module that defines a simple class which defines a rectangle
+"""
+
+
+class Rectangle:
+    """An empty rectangle class """
+    pass
